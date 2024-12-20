1ff6ceb791bc504d4acf49934494b5a6c52e5169
solomon <byersolomon@gmail.com>
2024-12-19 20:58:13 -0500

    saving

--- app.py
+++ app.py
@@ -31,7 +31,15 @@ def get_lyrics(song_title, artist_name):
         search_url = f"https://www.google.com/search?q={search_query}"
 
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Referer": "https://www.google.com",
+            "DNT": "1",  # Do Not Track Request Header
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache"
         }
 
         try:
